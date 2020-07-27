add584e94c27b5f381e57b7360e3031c737c8aec
Awtanabe <panser098@gmail.com>
2020-07-28 00:50:13 +0900

    mock count

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -1,12 +1,54 @@
-RSpec.describe 'a random double' do
-  it 'only allows defined methods to be invoked' do
-    # stuntman = double('Mr. Danger', fall_off_ladder: 'Ouch', light_on_fire: true)
-    # expect(stuntman.fall_off_ladder).to eq('Ouch')
-    # expect(stuntman.light_on_fire).to eq(true)
-
-    stuntman = double('Mr denger')
-    allow(stuntman).to receive_messages(fall_off_ladder: 'Outch',light_on_fire: true)
-    expect(stuntman.fall_off_ladder).to eq('Outch')
-    expect(stuntman.light_on_fire).to eq(true)
+class Actor
+  def initialize(name)
+    @name = name
   end
+  def ready?
+    sleep(3)
+    true
+  end
+  def act
+    "I love you baby"
+  end
+
+  def fall_off_ladder
+    "Call may agent"
+  end
+  def light_on_fire
+    false
+  end
+end
+
+class Movie
+  attr_reader :actor
+
+  def initialize(actor)
+    @actor = actor
+  end
+
+  def start_shooting
+    if actor.ready?
+      actor.act
+      actor.fall_off_ladder
+      actor.light_on_fire
+    end
+  end
+end
+
+# actor = Actor.new("Brad paid")
+# movie = Movie.new(actor)
+# movie.start_shooting
+
+RSpec.describe Movie do
+  let(:stundman){ double('Mr danger', ready?: true, act: "call my aget", fall_off_ladder: 'sure', light_on_fire: true)}
+  subject { described_class.new(stundman)}
+
+    describe 'ddd' do
+      it 'test' do
+        expect(stundman).to receive(:light_on_fire).once
+        expect(stundman).to receive(:light_on_fire).exactly(1).times
+        expect(stundman).to receive(:light_on_fire).at_least(1).times
+        expect(stundman).to receive(:light_on_fire).at_most(1).times
+        subject.start_shooting
+      end
+    end
 end
