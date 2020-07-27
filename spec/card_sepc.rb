d526d74b90021500d669c9c73b088df9d7aa99bf
Awtanabe <panser098@gmail.com>
2020-07-27 23:45:14 +0900

    lib ディレクトリ作成

--- spec/card_sepc.rb
+++ spec/card_sepc.rb
@@ -1,7 +1,14 @@
+require_relative '../lib/Card'
+
 RSpec.describe 'Card' do
   it 'has a type' do
-    card = Card.new('Ace of Spades')
-    expect(card.type).to eq('Ace of Spade')
+    card = Card.new('Ace of Spades','Ace')
+    expect(card.rank).to eq('Ace of Spades')
+  end
+
+  it 'has a rank' do
+    card = Card.new('Ace of Spades','Ace')
+    expect(card.suit).to eq('Ace')
   end
 end
 
