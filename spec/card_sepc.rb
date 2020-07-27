fa8fa5211dc03711569aad1d4ba1501a528b2b16
Awtanabe <panser098@gmail.com>
2020-07-27 23:47:10 +0900

    重複削除 before

--- spec/card_sepc.rb
+++ spec/card_sepc.rb
@@ -1,14 +1,16 @@
 require_relative '../lib/Card'
 
 RSpec.describe 'Card' do
+  before do
+    @card = Card.new('Ace of Spades','Ace')    
+  end
+
   it 'has a type' do
-    card = Card.new('Ace of Spades','Ace')
-    expect(card.rank).to eq('Ace of Spades')
+    expect(@card.rank).to eq('Ace of Spades')
   end
 
   it 'has a rank' do
-    card = Card.new('Ace of Spades','Ace')
-    expect(card.suit).to eq('Ace')
+    expect(@card.suit).to eq('Ace')
   end
 end
 
