6925edac983be5737055457a80694d98e72dd83d
Awtanabe <panser098@gmail.com>
2020-07-28 01:15:02 +0900

    sharec exemples

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -38,21 +38,34 @@ end
 # movie = Movie.new(actor)
 # movie.start_shooting
 
-class Prince
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
+RSpec.shared_examples 'a Ruby object with three elements' do
+  it 'returns the number of items' do
+    expect(subject.length).to eq(3)
   end
 end
 
-RSpec.describe Prince do
-  subject { described_class.new('Boris') }
-  let(:louis) { described_class.new('Louis') }
+RSpec.describe Array do
+  subject { [1, 2, 3] }
+  include_examples 'a Ruby object with three elements'
+end
 
-  it 'represents a great person' do
-    expect(subject.name).to eq('Boris')
-    expect(louis.name).to eq('Louis')
+RSpec.describe String do
+  subject { 'abc' }
+  include_examples 'a Ruby object with three elements'
+end
+
+RSpec.describe Hash do
+  subject {{ a: 1, b: 2, c: 3 }}
+  include_examples 'a Ruby object with three elements'
+end
+
+class SausageLink
+  def length
+    3
   end
 end
 
+RSpec.describe SausageLink do
+  subject { described_class.new }
+  include_examples 'a Ruby object with three elements'
+end
